4c9c5a858b6b83bedf37a8d29f79afc90a50dadd
Alex Magar <lx.mgr07@gmail.com>
2018-11-20 13:34:06 +0545

    makeDecision added

--- src/controller/ControlledScene.java
+++ src/controller/ControlledScene.java
@@ -4,7 +4,7 @@ public interface ControlledScene {
 	
 	//this method will allow the injection of the Parent ScenePane
 	public void setSceneParent(SceneController sceneParent);
-	
+
 	public void makeDecision(String output);
 
 
